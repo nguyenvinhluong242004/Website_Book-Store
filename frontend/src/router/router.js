862a8a566ee9db939ba7893a647ff221c6dfb977
22120209 <minhpqdp02@gmail.com>
2024-12-11 16:54:27 +0700

    them duongw dan cho trang dang kys

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DetailAccount from "../components/vue-component/DetailAccount.vue";
 import Home from "../components/vue-component/Home.vue";
+import Register from "../components/vue-component/Register.vue";
+
 
 const routes = [
   { path: "/", component: Home, name: "Home" },
   { path: "/account", component: DetailAccount, name: "DetailAccount" },
+  { path: "/register", component: Register, name: "Register" },
 ];
+
 
 const router = createRouter({
   history: createWebHistory(),
