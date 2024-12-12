a3c752665f361c75319ea9cc67ea3c324aba99c1
22120209 <minhpqdp02@gmail.com>
2024-12-13 02:47:13 +0700

    them slide cho anh trong component book

--- frontend/src/components/vue-component/Book.vue
+++ frontend/src/components/vue-component/Book.vue
@@ -7,7 +7,33 @@
     <div class="book-view-intro">
       <div class="bool-col-slide">
         <div class="bool-img">
-          <img src="../../../public/IMG/dulieu.jpg" alt="" />
+          <div class="book-body-slide">
+            <i class="fas fa-angle-left book-btn" @click="move_left()"></i>
+            <div class="book-view-frame">
+              <div
+                class="book-flex-slide"
+                :style="{ transform: `translateX(${x}px)` }"
+              >
+                <div v-for="(image, index) in images" :key="index">
+                  <img :src="image" width="300px" height="500px" alt="book" />
+                </div>
+              </div>
+            </div>
+            <i class="fas fa-angle-right book-btn" @click="move_right()"></i>
+          </div>
+          <div class="book-body-small mt-5">
+            <div class="book-small-img">
+              <div
+                v-for="(image, index_bot) in images"
+                :key="index_bot"
+                class="book-highlight"
+                :class="{ active: activeImg === index_bot }"
+                @click="move_img(index_bot)"
+              >
+                <img :src="image" width="70px" height="100px" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
         <div class="book-infor-product">Thong tin san pham</div>
       </div>
@@ -28,19 +54,12 @@
       không chỉ giúp cải thiện các mối quan hệ cá nhân mà còn là chìa khóa để
       thành công trong công việc và cuộc sống.
     </div>
-   
-    <div class="book-see-more">
-        Xem them
-    </div>
-    <div class="book-row-bot">
-        <div class="book-review"> </div>
-        <div class="book-same">
-            
-        </div>
-    </div>
-   
 
-   
+    <div class="book-see-more">Xem them</div>
+    <div class="book-row-bot">
+      <div class="book-review"></div>
+      <div class="book-same"></div>
+    </div>
   </div>
 </template>
 
@@ -50,8 +69,35 @@ import "../css-component/book.css";
 export default {
   name: "NavApp",
   data() {
-    return {};
+    return {
+      images: [
+        "/IMG/caigia.jpg",
+        "/IMG/cam.jpg",
+        "/IMG/do.jpg",
+        "/IMG/gioi.jpg",
+        "/IMG/philytri.jpg",
+      ],
+      x: 0,
+      activeImg: 0,
+    };
   },
-  methods: {},
+  methods: {
+    move_img(index) {
+      this.activeImg = index;
+      this.x = index * -310;
+    },
+    move_left() {
+      if (this.activeImg != 0) {
+        this.activeImg -= 1;
+        this.x += 310;
+      }
+    },
+    move_right() {
+      if (this.activeImg < this.images.length-1) {
+        this.activeImg += 1;
+        this.x -= 310;
+      }
+    },
+  },
 };
 </script>
