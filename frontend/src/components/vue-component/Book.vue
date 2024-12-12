78ff5b0971f914bc940263bb179eb46e9567de72
22120209 <minhpqdp02@gmail.com>
2024-12-13 00:22:16 +0700

    Them component chi tiet book kem css

---
+++ frontend/src/components/vue-component/Book.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="book-container">
+    <div class="book-name">
+      Code Breaker - Viet Lai Ma Su Song : Chinh sua gen loai nguoi
+    </div>
+    <hr />
+    <div class="book-view-intro">
+      <div class="bool-col-slide">
+        <div class="bool-img">
+          <img src="../../../public/IMG/dulieu.jpg" alt="" />
+        </div>
+        <div class="book-infor-product">Thong tin san pham</div>
+      </div>
+      <div class="book-col-price">
+        <div class="book-detail">Gia ban:</div>
+      </div>
+      <div class="book-col-infor">
+        <div class="book-extra">Tac gia:</div>
+      </div>
+    </div>
+    <hr />
+    <div class="book-text-book">
+      "Đắc Nhân Tâm" là cuốn sách nổi tiếng của Dale Carnegie, được xem như một
+      trong những tác phẩm kinh điển về nghệ thuật giao tiếp và đối nhân xử thế.
+      Qua những câu chuyện thực tế và các nguyên tắc dễ áp dụng, cuốn sách hướng
+      dẫn cách thấu hiểu, tạo dựng mối quan hệ và ảnh hưởng tích cực đến người
+      khác. Với thông điệp cốt lõi là "hiểu mình để hiểu người," "Đắc Nhân Tâm"
+      không chỉ giúp cải thiện các mối quan hệ cá nhân mà còn là chìa khóa để
+      thành công trong công việc và cuộc sống.
+    </div>
+   
+    <div class="book-see-more">
+        Xem them
+    </div>
+    <div class="book-row-bot">
+        <div class="book-review"> </div>
+        <div class="book-same">
+            
+        </div>
+    </div>
+   
+
+   
+  </div>
+</template>
+
+<script>
+import "../css-component/book.css";
+
+export default {
+  name: "NavApp",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
