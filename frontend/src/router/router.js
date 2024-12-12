8af522f4ac2d197aaadce56068709cba657fa4a7
22120209 <minhpqdp02@gmail.com>
2024-12-12 17:57:22 +0700

    them duong dan cho router toi profile

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import DetailAccount from "../components/vue-component/DetailAccount.vue";
 import Home from "../components/vue-component/Home.vue";
 import Register from "../components/vue-component/Register.vue";
+import Profile from "../components/vue-component/Profile.vue";
 
 
 const routes = [
   { path: "/", component: Home, name: "Home" },
   { path: "/account", component: DetailAccount, name: "DetailAccount" },
   { path: "/register", component: Register, name: "Register" },
+  { path: "/profile", component: Profile, name: "Profile" },
 ];
 
 
