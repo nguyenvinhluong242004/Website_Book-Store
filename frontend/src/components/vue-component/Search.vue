9654325fd5ec3997f153dc289b90b47f63eb76a2
22120187 <tranloc200415@gmail.com>
2024-12-20 00:01:20 +0700

    temp-search
    Trang search tạm thời đợi làm rõ thêm yêu cầu

--- frontend/src/components/vue-component/Search.vue
+++ frontend/src/components/vue-component/Search.vue
@@ -1,79 +1,194 @@
 <template>
-  <div class="home-container">
-    <div class="ctn-row">
-      <PosterSlide :posters="posters" />
+  <div class="search-container">
+    <div class="filter-box">
+      <div class="filter-box-title">Bộ lọc tìm kiếm</div>
+
+      <div class="filter-cate">
+        <div class="fw-bold">Thể loại</div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="cb1" />
+          <label class="form-check-label" for="cb1"> Siêu nhiên </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="cb2" />
+          <label class="form-check-label" for="cb2"> Kinh dị </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="cb3" />
+          <label class="form-check-label" for="cb3"> Tình cảm </label>
+        </div>
+      </div>
+
+      <div class="filter-cate">
+        <div class="fw-bold">Mức giá</div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb0"
+            checked
+          />
+          <label class="form-check-label" for="rb0"> Tất cả mức giá</label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb1"
+          />
+          <label class="form-check-label" for="rb1"> 0đ - 50,000đ </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb2"
+          />
+          <label class="form-check-label" for="rb2"> 50,000đ - 100,000đ </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb2"
+          />
+          <label class="form-check-label" for="rb3">
+            100,000đ - 250,000đ
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb2"
+          />
+          <label class="form-check-label" for="rb4">
+            250,000đ - 500,000đ
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterPrice"
+            id="rb2"
+          />
+          <label class="form-check-label" for="rb5"> 500,000đ trở lên </label>
+        </div>
+      </div>
     </div>
-    <div class="ctn-row">
-      <TopSeller :products_top="products_top" />
-    </div>
-    <div class="ctn-row">
-      <CategoryBook
-        :products_skill="products_skill"
-        :poster_skill="poster_skill"
-        :products_family="products_family"
-        :poster_family="poster_family"
-        :products_science="products_science"
-        :poster_science="poster_science"
-        :products_literature="products_literature"
-        :poster_literature="poster_literature"
-      />
+
+    <div class="search-result-box">
+      <div class="search-result-title">
+        Kết quả tìm kiếm <span class="text-primary">Hello</span>
+      </div>
+
+      <div class="filter-order-box">
+        <span class="text-secondary">Sắp xếp theo</span>
+        <input
+          type="radio"
+          class="btn-check"
+          name="filterOrder"
+          id="rbnew"
+          :checked="selectedRadio === 'new'"
+          @click="toggleFilter('new')"
+        />
+        <label class="btn btn-outline-primary" for="rbnew">Mới nhất ★</label>
+
+        <input
+          type="radio"
+          class="btn-check"
+          name="filterOrder"
+          id="rbhot"
+          :checked="selectedRadio === 'hot'"
+          @click="toggleFilter('hot')"
+        />
+        <label class="btn btn-outline-warning" for="rbhot">Bán chạy HOT</label>
+
+        <input
+          type="radio"
+          class="btn-check"
+          name="filterOrder"
+          id="rbpricel2h"
+          :checked="selectedRadio === 'pricel2h'"
+          @click="toggleFilter('pricel2h')"
+        />
+        <label class="btn btn-outline-success" for="rbpricel2h"
+          >Giá thấp đến cao ▲</label
+        >
+
+        <input
+          type="radio"
+          class="btn-check"
+          name="filterOrder"
+          id="rbpriceh2l"
+          :checked="selectedRadio === 'priceh2l'"
+          @click="toggleFilter('priceh2l')"
+        />
+        <label class="btn btn-outline-danger" for="rbpriceh2l"
+          >Giá cao đến thấp ▼</label
+        >
+      </div>
+
+      <div class="search-result-list">
+        <ProductCard
+          v-for="(product, index) in products_skill"
+          :key="index"
+          :img="product.img"
+          :name="product.name"
+          :old_price="product.old_price"
+          :new_price="product.new_price"
+          :type_money="product.type_money"
+        />
+
+        <ProductCard
+          v-for="(product, index) in products_skill"
+          :key="index"
+          :img="product.img"
+          :name="product.name"
+          :old_price="product.old_price"
+          :new_price="product.new_price"
+          :type_money="product.type_money"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import "../css-component/home.css";
-import PosterSlide from "./PosterSlide.vue";
-import TopSeller from "./TopSeller.vue";
-import CategoryBook from "./CategoryBook.vue";
+import "../css-component/search.css";
+
+import ProductCard from "./ProductCard.vue";
 
 export default {
   name: "SearchPage",
   components: {
-    PosterSlide,
-    TopSeller,
-    CategoryBook,
+    ProductCard,
   },
   data() {
     return {
-      posters: [],
-      poster_skill: "",
-      poster_family: "",
-      poster_science: "",
-      poster_literature: "",
-
-      products_top: [],
+      selectedRadio: null,
       products_skill: [],
-      products_family: [],
-      products_science: [],
-      products_literature: [],
-
-      categories: [],
-      subcategories: [],
     };
   },
-  //   computed: {
-  //     isDetailAccountPage() {
-  //       // Kiểm tra nếu đường dẫn hiện tại là `/account`
-  //       return this.$route.path === "/";
-  //     },
-  //   },
   mounted() {
     fetch("/DATABASE/data.json")
       .then((response) => response.json())
       .then((data) => {
-        this.posters = data.posters;
-        this.products_top = data.products_top;
         this.products_skill = data.products_skill;
-        this.poster_skill = data.poster_skill;
-        this.poster_science = data.poster_science;
-        this.products_science = data.products_science;
-        this.products_family = data.products_family;
-        this.products_literature = data.products_literature;
-        this.poster_family = data.poster_family;
-        this.poster_literature = data.poster_literature;
       })
       .catch((error) => console.error("Có lỗi xảy ra:", error));
+  },
+  methods: {
+    toggleFilter(value) {
+      // Kiểm tra nếu giá trị đã chọn thì hủy chọn
+      this.selectedRadio = this.selectedRadio === value ? null : value;
+    },
   },
 };
 </script>
