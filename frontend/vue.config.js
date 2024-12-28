348827ab1eb79abfb367ba0bea1f7f46e93723f9
22120209 <minhpqdp02@gmail.com>
2024-12-29 02:44:37 +0700

    done api for detail book

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,13 @@
-
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {  // Tất cả các yêu cầu bắt đầu với `/api` sẽ được chuyển tiếp
+        target: 'http://localhost:8888', // Địa chỉ của backend (nếu backend của bạn chạy trên 8080)
+        changeOrigin: true, // Đổi Origin trong request header
+        pathRewrite: { '^/api': '' }, // Xóa `/api` trong URL trước khi gửi yêu cầu tới backend
+      },
+    },
+  },
+});
