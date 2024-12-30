287c740d756ddd5db6d7874e06895f40b2a3459c
22120209 <minhpqdp02@gmail.com>
2024-12-31 01:54:56 +0700

    fix after pull

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -32,4 +32,11 @@ module.exports = defineConfig({
     host: 'localhost',
     allowedHosts: 'all', // Chấp nhận tất cả các hosts (có thể thay thế theo nhu cầu)
   },
+  proxy: {
+    '/api': {  // Tất cả các yêu cầu bắt đầu với `/api` sẽ được chuyển tiếp
+      target: 'http://localhost:8888', // Địa chỉ của backend (nếu backend của bạn chạy trên 8080)
+      changeOrigin: true, // Đổi Origin trong request header
+      pathRewrite: { '^/api': '' }, // Xóa `/api` trong URL trước khi gửi yêu cầu tới backend
+    },
+  },
 });
