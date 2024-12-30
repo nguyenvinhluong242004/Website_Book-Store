707b0695fd5d7a59a3eedff69d62a07bad94c17e
22120198 <trinhnguyenluong182306@gmail.com>
2024-12-30 17:46:56 +0700

    Change to VN message and modify time jwt

--- backend/controllers/auth/refresh.controller.js
+++ backend/controllers/auth/refresh.controller.js
@@ -4,23 +4,18 @@ const jwt = require('jsonwebtoken');
 // [GET]: /refresh
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
-    console.log('NL4');
-    console.log('COOKIES: ', req.cookies);
+    console.log('COOKIES REFRESH: ', req.cookies);
     if(!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
-    console.log('NL0');
 
     const foundUser = await userModel.getUserByRefreshToken(refreshToken);
     if(!foundUser) return res.sendStatus(403);
     
-    console.log('NL3');
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            console.log('NL1');
             if(err || foundUser.email !== decoded.email) return res.sendStatus(403);
-            console.log('NL2');
             const role = Object.values(foundUser.role);
             const accessToken = jwt.sign(
                 {
@@ -30,7 +25,7 @@ const handleRefreshToken = async (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '1m' }
+                { expiresIn: '1h' }
             )
             return res.status(200).json({ accessToken });
         }
--- backend/controllers/auth/register.controller.js
+++ backend/controllers/auth/register.controller.js
@@ -6,18 +6,18 @@ const handleNewUser = async (req, res) => {
     // console.log('BODY CLIENT SENT: ', req.body);
     const { email, name, phone, password, confirmedPassword } = req.body;
     // Mã 400: Lỗi phía client
-    if (!email || !password || !confirmedPassword || !name || !phone) return res.status(400).json({ 'message': 'All field are required. ' });
+    if (!email || !password || !confirmedPassword || !name || !phone) return res.status(400).json({ 'message': 'Các trường đều phải được nhập' });
 
     // Kiểm tra password và confirmedPassword
     if (password !== confirmedPassword) {
-        return res.status(400).json({ 'message': 'Password and Confirmed Password do not match.' });
+        return res.status(400).json({ 'message': 'Mật khẩu không trùng khớp' });
     }
 
     try {
         // Kiểm tra xem email đã tồn tại chưa
-        const existingUser = await userModel.getUserByEmail(email);
+        const existingUser = await userModel.getUserByEmail(email); 
         if (existingUser) {
-            return res.status(409).json({ 'message': 'Email has existed.' }); // Mã 409: Conflict
+            return res.status(409).json({ 'message': 'Email đã được đăng ký' }); // Mã 409: Conflict
         }
 
         // Mã hóa mật khẩu
@@ -27,7 +27,7 @@ const handleNewUser = async (req, res) => {
         const newUser = await userModel.createUser(email, name, phone, userRole, hashedPassword);
 
         // Mã 201: Tạo thành công
-        res.status(201).json({ 'success': `New user with email ${email} created!`, 'user': newUser });
+        res.status(201).json({ 'success': `Tài khoản đã được tạo thành công`});
     } catch (err) {
         res.status(500).json({ 'message': err.message });
     }
