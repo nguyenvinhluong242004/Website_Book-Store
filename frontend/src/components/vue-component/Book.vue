30f33008216b295aa0735c22ed62836993957ecd
22120187 <tranloc200415@gmail.com>
2025-01-05 09:24:22 +0700

    update add to cart in Book
    thay đổi lại cái thêm vào giỏ hàng trong book.vue

--- frontend/src/components/vue-component/Book.vue
+++ frontend/src/components/vue-component/Book.vue
@@ -533,7 +533,6 @@ export default {
       quantityOfBook: 1,
       book: [],
       id: null,
-      quantity: 1,
       status: false,
       isExpanded: false,
       isModalVisible: false,
@@ -692,8 +691,8 @@ export default {
     async addToCart() {
       try {
         const response = await axiosInstance.post("/cart/add", {
-          id_book: "1",
-          quantity: this.quantity,
+          id_book: `${this.$route.query.id_book}`,
+          quantity: this.quantityOfBook,
         });
 
         if (response.status === 200) {
