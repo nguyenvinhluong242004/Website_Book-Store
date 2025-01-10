badcd15ed15a94016ed7e2446e56d63c8972edc2
22120209 <minhpqdp02@gmail.com>
2025-01-10 08:24:10 +0700

    fix image for top and category

--- frontend/src/components/vue-component/CategoryBook.vue
+++ frontend/src/components/vue-component/CategoryBook.vue
@@ -40,7 +40,7 @@
         <ProductCard
           v-for="(product, index) in kinhTe"
           :key="index"
-          :img="product.images"
+          :img="product.images[0]"
           :name="product.book_name"
           :old_price="product.list_price"
           :new_price="product.discounted_price"
@@ -57,7 +57,7 @@
         <ProductCard
           v-for="(product, index) in tamLy"
           :key="index"
-          :img="product.images"
+          :img="product.images[0]"
           :name="product.book_name"
           :old_price="product.list_price"
           :new_price="product.discounted_price"
@@ -74,7 +74,7 @@
         <ProductCard
           v-for="(product, index) in vanHoc"
           :key="index"
-          :img="product.images"
+          :img="product.images[0]"
           :name="product.book_name"
           :old_price="product.list_price"
           :new_price="product.discounted_price"
@@ -91,7 +91,7 @@
         <ProductCard
           v-for="(product, index) in tieuThuyet"
           :key="index"
-          :img="product.images"
+          :img="product.images[0]"
           :name="product.book_name"
           :old_price="product.list_price"
           :new_price="product.discounted_price"
@@ -143,6 +143,7 @@ export default {
           this.tamLy = response.data.psychologyBooks;
           this.vanHoc = response.data.literaryBooks;
           this.tieuThuyet = response.data.novelBooks;
+          //console.log(this.tieuThuyet);
         }
       } catch (error) {
         this.error = "Không thể lấy thông tin sách!";
