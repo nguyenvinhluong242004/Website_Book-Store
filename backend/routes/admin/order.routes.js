3d2787a04ded9e255aec2ab160f2e5d9894b82c8
22120198 <trinhnguyenluong182306@gmail.com>
2025-01-11 23:18:02 +0700

    Add API about order in admin site

--- backend/routes/admin/order.routes.js
+++ backend/routes/admin/order.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const orderController = require('../../controllers/admin/order.controller');
 
 router.get('/detail/:id_order', orderController.getDetailOrder);
+router.get('/status-order', orderController.getOrdersByStatus);
 router.patch('/update-status', orderController.updateStatusOfOrder);
 router.get('/', orderController.getAllOrders);
 
