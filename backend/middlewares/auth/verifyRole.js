ea98fbe95f289b2fe1ef7669d076f66946628326
22120198 <trinhnguyenluong182306@gmail.com>
2025-01-13 02:33:35 +0700

    Fix bug verify role

--- backend/middlewares/auth/verifyRole.js
+++ backend/middlewares/auth/verifyRole.js
@@ -4,12 +4,12 @@ const verifyRole = (role) => {
     return (req, res, next) => {
         if(!req?.role) return res.sendStatus(401); // 401: Unauthorized
         console.log('YOUR ROLE: ', req.role);
-        console.log('REQUIRED ROLE: ', role);
+        console.log('REQUIRED ROLE: ', ROLES_LIST[role]);
         
         const userRoleValue = req.role;
         const requiredRoleValue = ROLES_LIST[role];
 
-        if (userRoleValue !== requiredRoleValue) {
+        if (!userRoleValue || userRoleValue < requiredRoleValue) {
             return res.sendStatus(403); // 403: Forbidden
         }
         next();
