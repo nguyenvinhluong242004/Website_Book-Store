74e663573ed2ccd90f1f0da55c3f1266e1ba9b89
22120209 <minhpqdp02@gmail.com>
2025-01-13 21:25:51 +0700

    code hard

---
+++ frontend/src/components/vue-component/Colab.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container my-5">
+    <header class="text-center mb-4">
+      <h1>Hợp tác kinh doanh</h1>
+      <h2>Bách khoa sách</h2>
+      <p class="text-muted">22/02/2024</p>
+    </header>
+    <section class="mb-4">
+      <p>
+        Thành lập ngày <strong>21/1/2005</strong>, đến nay BKS đã trở
+        thành một trong những thương hiệu hàng đầu tại Việt Nam về mảng sách
+        quản trị kinh doanh và giáo dục, tư duy. Đến nay, BKS không
+        ngừng lớn mạnh với nhiều dòng sách mới và trở thành người bạn thân thiết
+        của hàng triệu độc giả Việt Nam.
+      </p>
+    </section>
+    <section>
+      <h3>Thông tin liên hệ cho các hoạt động kinh doanh:</h3>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Kênh TMĐT</h5>
+          <p class="card-text"><strong>Chu Đức Tấn</strong></p>
+          <p>ĐT: 036 841 0213</p>
+          <p>
+            Email:
+            <a href="mailto:tan.chu@bks.vn">tan.chu@bks.vn</a>
+          </p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Kênh GT-MT</h5>
+          <p class="card-text"><strong>Nguyễn Thu Hương</strong></p>
+          <p>ĐT: 093 859 5866</p>
+          <p>
+            Email:
+            <a href="mailto:huong.nt@bks.vn">huong.nt@bks.vn</a>
+          </p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Kênh Trường học</h5>
+          <p class="card-text"><strong>Trần Thị Hiên</strong></p>
+          <p>ĐT: 097 252 6572</p>
+          <p>
+            Email:
+            <a href="mailto:hien.tran@bks.vn">hien.tran@bks.vn</a>
+          </p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">
+            Hợp tác xuất bản, tư vấn xây dựng tủ sách doanh nghiệp
+          </h5>
+          <p>Hotline: 093 613 6060</p>
+          <p>
+            Email: <a href="mailto:htxb@bks.vn">htxb@bks.vn</a>
+          </p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Bản quyền</h5>
+          <p>
+            Email:
+            <a href="mailto:copyright@bks.vn">copyright@bks.vn</a>
+          </p>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="card-body">
+          <h5 class="card-title">Truyền thông - Marketing</h5>
+          <p>
+            Email:
+            <a href="mailto:mkt1.bks@gmail.com"
+              >mkt1.bks@gmail.com</a
+            >
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IntroPage",
+
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
