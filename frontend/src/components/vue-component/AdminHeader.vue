6d0b2ce9b69e7c2bbc48fde116f33a404afcf266
22120187 <tranloc200415@gmail.com>
2025-01-13 10:04:10 +0700

    logic for admin login
    check role để xét logic có cho vào admin hay không

--- frontend/src/components/vue-component/AdminHeader.vue
+++ frontend/src/components/vue-component/AdminHeader.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="admin-header">
-    <router-link to="/admin/dashboard"><div class="admin-header-logo-box text-primary">
-      <i class="fas fa-book"></i>
-      <div class="admin-header-logo-text">BÁCH KHOA SÁCH</div>
-    </div></router-link>
+    <router-link to="/admin/dashboard"
+      ><div class="admin-header-logo-box text-primary">
+        <i class="fas fa-book"></i>
+        <div class="admin-header-logo-text">BÁCH KHOA SÁCH</div>
+      </div></router-link
+    >
     <div class="col border-2 border-start border-end h-50"></div>
     <div class="admin-header-user-box">
       <img
@@ -11,13 +13,14 @@
         alt="Avatar"
         class="admin-header-avatar"
       />
-      <div class="text-primary fw-bold mx-3">Trần Thiên Lộc</div>
+      <div class="text-primary fw-bold mx-3">{{ name }}</div>
       <i
         class="fa-solid fa-arrow-right-from-bracket"
         :class="{ 'text-primary': hoverLogout, 'text-muted': !hoverLogout }"
+        style="cursor: pointer"
         @mouseenter="hoverLogout = true"
         @mouseleave="hoverLogout = false"
-        style="cursor: pointer"
+        @click="handleLogOut"
       ></i>
     </div>
   </div>
@@ -25,22 +28,71 @@
 
 <script>
 import "../css-component/admin-header.css";
-// import axiosInstance from "../../services/axiosInstance.js";
+import axiosInstance from "../../services/axiosInstance.js";
 
 export default {
   name: "AdminHeader",
   data() {
     return {
       hoverLogout: false,
+      name: null,
     };
   },
-  //   mounted() {
-  //     this.handleRouteChange(); // Thực hiện xử lý khi component được mount
-  //   },
-  //   watch: {
-  //     $route() {
-  //       this.handleRouteChange();
-  //     },
-  //   },
+  mounted() {
+    this.handleRouteChange(); // Thực hiện xử lý khi component được mount
+  },
+  watch: {
+    $route() {
+      this.handleRouteChange();
+    },
+  },
+  methods: {
+    async handleRouteChange() {
+      try {
+        // Gửi yêu cầu để lấy xem người dùng có đang đăng nhập không
+        const response = await axiosInstance.get("/account/profile");
+        if (response.status === 200) {
+          const user = response.data.user;
+          this.name = user.name;
+
+          // Nếu là người dùng thường thì đẩy về trang user
+          if (user.role === "1") {
+            alert('Bạn phải là admin để truy cập vào đường dẫn này!');
+            this.$router.push("/");
+          }
+        }
+      } catch (error) {
+        if (error.response) {
+          const status = error.response.status;
+          const message = error.response.data.message;
+
+          // Xử lý các mã lỗi cụ thể
+          if (status === 404) {
+            // Nếu lỗi là ko có người dùng hoặc không hợp lệ thì đẩy về login
+            this.$router.push("/login");
+          } else if (status === 500) {
+            alert(message);
+            this.$router.push("/login");
+          }
+        } else {
+          // Xử lý lỗi nếu không có phản hồi (chẳng hạn lỗi kết nối mạng)
+          alert("Lỗi mạng: Không thể kết nối đến server.");
+        }
+      }
+    },
+
+    async handleLogOut() {
+      try {
+        const response = await axiosInstance.post("/logout");
+        if (response.status === 204) {
+          localStorage.removeItem("accessToken");
+          this.$router.push("/login");
+        }
+      } catch (error) {
+        alert(error);
+        this.$router.push("/login");
+      }
+    },
+  },
 };
 </script>
