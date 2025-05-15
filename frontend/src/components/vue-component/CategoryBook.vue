e41b1ac712e2e958f0aefac1f5918e479c8ed031
22120197 <nguyenvinhluong242004@gmail.com>
2025-05-16 00:05:01 +0700

    Update url api

--- frontend/src/components/vue-component/CategoryBook.vue
+++ frontend/src/components/vue-component/CategoryBook.vue
@@ -136,7 +136,7 @@ export default {
     async getApiEconomy() {
       this.loading = true;
       try {
-        const response = await axios.get(`/api/book-economy`); // Lấy API qua proxy
+        const response = await axios.get(`https://book-store-backend-olvp.onrender.com/api/book-economy`); // Lấy API qua proxy
 
         if (response.data.success) {
           this.kinhTe = response.data.economyBooks;
@@ -151,7 +151,7 @@ export default {
     async getApiLiterary() {
       this.loading = true;
       try {
-        const response = await axios.get(`/api/book-literary`); // Lấy API qua proxy
+        const response = await axios.get(`https://book-store-backend-olvp.onrender.com/api/book-literary`); // Lấy API qua proxy
 
         if (response.data.success) {
           this.vanHoc = response.data.literaryBooks;
@@ -167,7 +167,7 @@ export default {
     async getApiNovel() {
       this.loading = true;
       try {
-        const response = await axios.get(`/api/book-novel`); // Lấy API qua proxy
+        const response = await axios.get(`https://book-store-backend-olvp.onrender.com/api/book-novel`); // Lấy API qua proxy
 
         if (response.data.success) {
           console.log(response.data);
@@ -183,7 +183,7 @@ export default {
     async getApiPsychology() {
       this.loading = true;
       try {
-        const response = await axios.get(`/api/book-psychology`); // Lấy API qua proxy
+        const response = await axios.get(`https://book-store-backend-olvp.onrender.com/api/book-psychology`); // Lấy API qua proxy
 
         if (response.data.success) {
           console.log(response.data);
--- frontend/src/components/vue-component/Search.vue
+++ frontend/src/components/vue-component/Search.vue
@@ -397,7 +397,7 @@ export default {
         this.typeSort = "";
       }
 
-      const url = `/api/search/filter?keyword=${this.searchQuery}&page=${this.page}&genre=${this.selectGenre}&age=${this.selectAge}&startPrice=${this.startPrice}&endPrice=${this.endPrice}&${this.sortBy}=${this.typeSort}`;
+      const url = `https://book-store-backend-olvp.onrender.com/api/search/filter?keyword=${this.searchQuery}&page=${this.page}&genre=${this.selectGenre}&age=${this.selectAge}&startPrice=${this.startPrice}&endPrice=${this.endPrice}&${this.sortBy}=${this.typeSort}`;
       console.log(url);
       axios
         .get(url)
@@ -419,7 +419,7 @@ export default {
     },
     async fetchGenres() {
       try {
-        const response = await axios.get("/api/genres"); // Thực hiện GET request
+        const response = await axios.get("https://book-store-backend-olvp.onrender.com/api/genres"); // Thực hiện GET request
         this.categories = response.data.genres;
       } catch (error) {
         console.error("Error fetching genres:", error);
@@ -446,7 +446,7 @@ export default {
       this.isLoading = true;
       try {
         const response = await axios.get(
-          `/api/search?keyword=${this.searchQuery}&page=${this.page}`
+          `https://book-store-backend-olvp.onrender.com/api/search?keyword=${this.searchQuery}&page=${this.page}`
         ); // Lấy API qua proxy
         if (response.data.success) {
           this.arrayBook = response.data.data;
